3e1f567ac48c266590b1a01950937a9d0fbc0678
Aline Sanches <96800792+alnsanches@users.noreply.github.com>
2022-08-11 14:56:36 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+    const btnAvancar = document.getElementById('btn-avancar'); 
+    const cartoes = document.querySelectorAll('.cartao'); 
+    let cartaoAtual = 0;
+    
+    function esconderCartaoSelecionado() {
+      const cartaoSelecionado = document.querySelector('.selecionado');
+      cartaoSelecionado.classList.remove('selecionado');
+    }
+    
+    function mostrarCartao() {
+        cartoes[cartaoAtual].classList.add('selecionado');  
+    }
+    
+    btnAvancar.addEventListener('click', function () {
+        if (cartaoAtual === cartoes.length - 1) return;
+        esconderCartaoSelecionado();
+        cartaoAtual++;
+        mostrarCartao();
+    });
+    
+        
+    const btnVoltar = document.getElementById('btn-voltar');     
+    btnVoltar.addEventListener('click', function () {
+        if (cartaoAtual === 0) return;
+        esconderCartaoSelecionado();
+        cartaoAtual--;
+        mostrarCartao();
+    });
